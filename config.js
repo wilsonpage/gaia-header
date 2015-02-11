957446a97b79339c20718c131eb48777b5453c61
Wilson Page <wilsonpage@me.com>
2015-02-11 08:16:22 +0100

    Add jspm support

---
+++ config.js
@@ -0,0 +1,15 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "font-fit": "github:gaia-components/font-fit@0.2.2",
+    "gaia-component": "github:gaia-components/gaia-component@0.3.4"
+  }
+});
+
